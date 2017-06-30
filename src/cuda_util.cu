ef0886fa90d089dec4bc53ce267667c1b0fd115f
Matthew Kerr <matthew.kerr@gmail.com>
2017-06-30 15:23:06 -0600

    Move print function to utilities.

--- src/cuda_util.cu
+++ src/cuda_util.cu
@@ -28,3 +28,19 @@ void curandcheck(curandStatus_t err){
     throw 20;
   }
 }
+
+void print_cuda_properties (int device_idx, FILE* stream) {
+  cudaDeviceProp prop;
+  cudaGetDeviceProperties (&prop,device_idx);
+  int nsms;
+  cudaDeviceGetAttribute (&nsms,cudaDevAttrMultiProcessorCount,device_idx);
+  fprintf (stream, "%s:\n",prop.name);
+  fprintf (stream, "maxThreadsPerBlock= %d\n",prop.maxThreadsPerBlock);
+  fprintf (stream, "maxThreadsDim[0]= %d\n",prop.maxThreadsDim[0]);
+  fprintf (stream, "maxThreadsDim[1]= %d\n",prop.maxThreadsDim[1]);
+  fprintf (stream, "maxThreadsDim[2]= %d\n",prop.maxThreadsDim[2]);
+  fprintf (stream, "maxGristream, dSize[0]= %d\n",prop.maxGridSize[0]);
+  fprintf (stream, "maxGridSize[1]= %d\n",prop.maxGridSize[1]);
+  fprintf (stream, "maxGridSize[2]= %d\n",prop.maxGridSize[2]);
+  fprintf (stream, "multiProcessors= %d\n",nsms);
+}
--- src/cuda_util.h
+++ src/cuda_util.h
@@ -1,6 +1,8 @@
 #include <cufft.h>
 #include <curand.h>
+#include <stdio.h>
 
 void cudacheck (cudaError_t err);
 void cufftcheck (cufftResult err);
 void curandcheck (curandStatus_t err);
+void print_cuda_properties (int device_idx, FILE* stream);
