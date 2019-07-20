481e3d6577c0eec1da3b0f4b9e92ee0d372151d3
Matthew Kerr <matthew.kerr@gmail.com>
2019-07-20 11:32:24 -0600

    Add a second klugey mode to record a burst of data.

--- src/recorder.py
+++ src/recorder.py
@@ -26,11 +26,29 @@ def send_trigger(trigger_struct):
 
 if __name__ == '__main__':
 
+    # works with two modes
+    # python recorder.py 10  -- record 10 seconds of data, writing out 1s/s
+    # python recorder.py 10 1 -- record 10s of data *AT ONCE* (burst)
+
     nsec = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        burst = True
+    else:
+        burst = False
+
+    if burst:
+        t0 = time.time() # time in Unix epoch
+        t1 = t0-nsec
+        t0,t1 = t1,t0
+        s = 'Burst mode record of %d seconds.'%nsec
+        print 't0=',t0,' t1=',t1
+        t = struct.pack('dd128s',t0,t1,s)
+        send_trigger(t)
+        sys.exit(0)
 
     for i in range(nsec):
         t0 = time.time() # time in Unix epoch
-        t1 = t0 + 0.1
+        t1 = t0 + 1e-6
         s = 'Recorded data segment number %02d.'%(i)
         print 't0=',t0,' t1=',t1
         t = struct.pack('dd128s',t0,t1,s)
